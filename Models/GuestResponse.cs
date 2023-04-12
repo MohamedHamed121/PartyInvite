96281b47da99570314bc06b4d1f4091bdea07368
MohamedHamed <MohamedHamed205>
2023-04-12 16:22:53 +0200

    first progress

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+	public class GuestResponse
+	{
+		[Required(ErrorMessage ="Please you should add you name ..")]
+		public string? Name  { get; set; }
+
+		[Required(ErrorMessage ="Please you should add you email ..")]
+		[EmailAddress]
+		public string? Email {get; set;}
+
+		[Required(ErrorMessage ="Please you should add you phone number ..")]
+		public string? Phone {get; set;}
+
+		[Required(ErrorMessage ="Please say weather you will attend or not...")]
+		public bool? WillAttend {get; set;}
+	}
+}
